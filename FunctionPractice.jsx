907812270dc70f487099911db6bc341317e1ae7a
Morris Bankston <mobanks10@gmail.com>
2017-02-14 11:46:46 -0500

    Update FunctionPractice.jsx

--- FunctionPractice.jsx
+++ FunctionPractice.jsx
@@ -4,9 +4,16 @@ function takeOrder(topping,crustType) {
   orderCount=orderCount+1;
 }
 function getSubTotal(itemCount){
-  return itemCount*7.5
-};
+  return itemCount*7.5;
+}
+function getTax(){
+  return getSubTotal(orderCount)*0.06;
+}
+function getTotal(){
+  return getSubTotal(orderCount) + getTax();
+}
 takeOrder('topping','crustType');
 takeOrder('crustType','crustType');
 takeOrder('topping','crustType');
 console.log(getSubTotal(orderCount));
+console.log(getTotal());
