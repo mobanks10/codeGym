9ed9770bb019ae82802008e2656f0fbb9b301565
Morris Bankston <mobanks10@gmail.com>
2017-02-14 20:02:17 -0500

    Create ScopePractice.jsx

---
+++ ScopePractice.jsx
@@ -0,0 +1,9 @@
+var laundryRoom='Basement';
+var mailRoom='Room 1A';
+function myApartment(){
+  var mailBoxNumber='Box 3';
+  laundryRoom='In-unit';
+  console.log('Mail box: '+mailBoxNumber+', Laundry: '+laundryRoom);
+}
+console.log('Laundry: '+laundryRoom+', Mail: '+mailRoom);
+myApartment();
