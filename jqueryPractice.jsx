e940c238cb19696753029c4331fe23f2fae92e9d
Morris Bankston <mobanks10@gmail.com>
2017-02-18 16:48:11 -0500

    Create jqueryPractice.jsx

---
+++ jqueryPractice.jsx
@@ -0,0 +1,16 @@
+function main() {
+  $('.skillset').hide();
+  $('.skillset').fadeIn(1000);
+  
+  $('.projects').hide();
+  
+  $('.projects-button').on('click', function() {
+		//$(this).next().slideToggle(400);
+    $('.projects').slideToggle(400);
+    $(this).toggleClass('active');
+    $(this).text('Projects Viewed');
+    $(this).next().toggle();
+	});
+}
+
+$(document).ready(main);
