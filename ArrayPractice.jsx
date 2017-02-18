194ddd5e0f22cfc781f34e1dba4cac8d19d1dfad
Morris Bankston <mobanks10@gmail.com>
2017-02-18 14:06:36 -0500

    Create ArrayPractice.jsx

---
+++ ArrayPractice.jsx
@@ -0,0 +1,16 @@
+var bucketList = ['chicken','beer','popcorn'];
+console.log(bucketList);
+
+var listItem=bucketList[0];
+console.log(listItem);
+
+var listItem=bucketList[2];
+
+var listItem=bucketList[3];
+console.log(bucketList.length);
+
+bucketList.push('bacon','scotch');
+console.log(bucketList);
+
+bucketList.pop();
+console.log(bucketList);
