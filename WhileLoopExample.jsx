e0ef52947cb9d27b17a38e3969ce069ed23fe784
Morris Bankston <mobanks10@gmail.com>
2017-02-18 14:56:08 -0500

    Create WhileLoopExample.jsx

---
+++ WhileLoopExample.jsx
@@ -0,0 +1,13 @@
+var cards = ['Diamond', 'Spade', 'Heart', 'Club'];
+
+var currentCard = 'Heart';
+
+while (currentCard !== 'Spade') {
+  console.log(currentCard);
+
+  var randomNumber = Math.floor(Math.random() * 4);
+
+  currentCard = cards[randomNumber];
+}
+
+console.log('Found a Spade!');
